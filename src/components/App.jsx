e14331db5b982842b43b2a7c1e745bba9bd7c7b7
Kilkenni <hpittman89@outlook.com>
2022-02-23 21:16:12 +0200

    commenting out dummy contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,12 @@ import ContactForm from "./ContactForm";
 import Filter from "./Filter";
 import ContactList from "./ContactList";
 
-const DUMMY_CONTACTS = [
-  {id: 'KindLady', name: 'Athene Margoulis', number: '459-12-56'},
-  {id: 'Nucl3arSnake', name: 'Francis Pritchard', number: '443-89-12'},
-  {id: 'FlyGirl', name: 'Faridah Malik', number: '645-17-79'},
-  {id: 'TyphoonMaster', name: 'Vasili Shevchenko', number: '227-91-26'},
-];
+// const DUMMY_CONTACTS = [
+//   {id: 'KindLady', name: 'Athene Margoulis', number: '459-12-56'},
+//   {id: 'Nucl3arSnake', name: 'Francis Pritchard', number: '443-89-12'},
+//   {id: 'FlyGirl', name: 'Faridah Malik', number: '645-17-79'},
+//   {id: 'TyphoonMaster', name: 'Vasili Shevchenko', number: '227-91-26'},
+// ];
 
 export default class App extends React.Component {
   state = {
