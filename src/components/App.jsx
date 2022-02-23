630910015aa116033e9869ed18fabaf1c29b2155
Kilkenni <hpittman89@outlook.com>
2022-02-23 21:13:13 +0200

    added saving contacts to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,38 @@ import ContactForm from "./ContactForm";
 import Filter from "./Filter";
 import ContactList from "./ContactList";
 
+const DUMMY_CONTACTS = [
+  {id: 'KindLady', name: 'Athene Margoulis', number: '459-12-56'},
+  {id: 'Nucl3arSnake', name: 'Francis Pritchard', number: '443-89-12'},
+  {id: 'FlyGirl', name: 'Faridah Malik', number: '645-17-79'},
+  {id: 'TyphoonMaster', name: 'Vasili Shevchenko', number: '227-91-26'},
+];
+
 export default class App extends React.Component {
   state = {
-    contacts: [
-      {id: 'KindLady', name: 'Athene Margoulis', number: '459-12-56'},
-      {id: 'Nucl3arSnake', name: 'Francis Pritchard', number: '443-89-12'},
-      {id: 'FlyGirl', name: 'Faridah Malik', number: '645-17-79'},
-      {id: 'TyphoonMaster', name: 'Vasili Shevchenko', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '', //search filter
+  }
+
+  componentDidMount() {
+    //localStorage.setItem("contacts", JSON.stringify(DUMMY_CONTACTS)); //pre-fill localStorage
+    const storedContacts = JSON.parse(localStorage.getItem("contacts"));
+
+    if (storedContacts !== null) {
+      this.setState({ contacts: storedContacts }); //restoring state from localStorage
+    }
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      if (this.state.contacts && this.state.contacts.length > 0) {
+        localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+      }
+      else { //if no contacts remain, clean localStorage entirely
+        localStorage.removeItem("contacts");
+      }    
+    } 
   }
 
   addContact = (newContact) => {
